d451e6277aeb4c39f8487d51f0d4859e64cb3164
geoncheollee <day071211@gmail.com>
2015-09-30 04:48:46 +0900

    1. 모집리스트 아이템 세로 크기 5dp 줄임 (원 찌그러짐 완화) 2. 모집리스트 팝업 참여하기 구현

--- app/src/main/java/vivz/slidenerd/agriculture/recruit/RecruitPopupActivity.java
+++ app/src/main/java/vivz/slidenerd/agriculture/recruit/RecruitPopupActivity.java
@@ -3,6 +3,9 @@ package vivz.slidenerd.agriculture.recruit;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,10 +16,16 @@ import android.view.Window;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
 
 import vivz.slidenerd.agriculture.R;
 
@@ -33,8 +42,18 @@ public class RecruitPopupActivity extends Activity implements View.OnClickListen
     WebView webvRecPopup;
 
     Button btnPhoneCall;
+    Button btnMissionJoin;
 
     String imgUrl = "http://218.150.181.131/seo/image/"; // 사진이 없을 경우 디폴트 사진 띄우는 경로
+
+    phpMissionJoin missionJoin;
+
+    // 참가 성공여부
+    public static final int joinSucceed = 1;
+    public static final int joinNoSucceed = 0;
+    public static final int joinAlreadyIn = 2;
+
+    JoinHandler myJoinHandler;
 
 
     @Override
@@ -57,17 +76,10 @@ public class RecruitPopupActivity extends Activity implements View.OnClickListen
         txtvRecruitNum.setText(item.getRecruitNum());
 
         btnPhoneCall = (Button)findViewById(R.id.btnPhoneCall);
+        btnMissionJoin = (Button)findViewById(R.id.btnMissionJoin);
 
-        btnPhoneCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.btnPhoneCall:
-                        startActivity(new Intent("android.intent.action.DIAL", Uri.parse("tel:"+item.getPhoneNum())));
-                        break;
-                }
-            }
-        });
+        btnPhoneCall.setOnClickListener(this);
+        btnMissionJoin.setOnClickListener(this);
 
         // 줄바꿈
         String lineEnding = item.getRecuritContent().replace("99line99end99", "\n");
@@ -99,6 +111,8 @@ public class RecruitPopupActivity extends Activity implements View.OnClickListen
 
         webvRecPopup.loadDataWithBaseURL(null, creHtmlBody(loadingURL), "text/html", "utf-8", null);
         Log.e("list image path", loadingURL);
+
+        myJoinHandler = new JoinHandler();
     }
 
     // 리스트 뷰 항목에 들어가는 웹뷰 이미지 화면을 웹뷰크기에 맞게 조절
@@ -115,7 +129,117 @@ public class RecruitPopupActivity extends Activity implements View.OnClickListen
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnPhoneCall:
+                startActivity(new Intent("android.intent.action.DIAL", Uri.parse("tel:"+item.getPhoneNum())));
+                break;
 
+            case R.id.btnMissionJoin:
+                missionJoin = new phpMissionJoin();
+                MissionItem missionItem = new MissionItem("geon", item.getIdRecruit());
+                missionJoin.execute("http://218.150.181.131/seo/phpMissionJoin.php?" + missionItem.toString());
+                break;
+        }
+    }
+
+    // 참가하기 php
+    public class phpMissionJoin extends AsyncTask<String, Integer,String> {
+
+        Message msg = myJoinHandler.obtainMessage(); // 참가 성공 실패 핸들러
+
+        @Override
+        protected String doInBackground(String... urls) {
+            StringBuilder jsonHtml = new StringBuilder();
+            String line ="";
+            try{
+                // 텍스트 연결 url 설정
+                URL url = new URL(urls[0]);
+                // 이미지 url
+                Log.e("tag", "url : " + urls[0]);
+                // URL 페이지 커넥션 객체 생성
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                // 연결되었으면.
+
+                if(conn != null){
+                    conn.setConnectTimeout(10000);
+                    conn.setUseCaches(false);
+                    // 연결되었음 코드가 리턴되면.
+                    Log.e("tag", "setUseCaches is false");
+                    if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                        for(;;){
+                            // 웹상에 보여지는 텍스트를 라인단위로 읽어 저장.
+                            line = br.readLine();
+                            if(line == null) break;
+                            // 저장된 텍스트 라인을 jsonHtml에 붙여넣음
+                            jsonHtml.append(line);
+                        }
+                        br.close();
+                    }
+                    conn.disconnect();
+                }
+                msg.what = joinSucceed;
+            } catch(Exception ex){
+                ex.printStackTrace();
+                msg.what = joinNoSucceed;
+            }
+            return jsonHtml.toString();
+        }
+
+        protected void onPostExecute(String str){
+
+            if ( !str.contains("1 record added") ) {
+                msg.what = joinNoSucceed;
+            }
+
+            if ( str.contains("key 'PRIMARY'")) {
+                msg.what = joinAlreadyIn;
+            }
+
+            myJoinHandler.sendMessage(msg);
+        }
+    }
+
+    // Handler 클래스
+    class JoinHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what) {
+                case joinSucceed:
+                    Toast.makeText(getApplicationContext(), "참가하였습니다.", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case joinNoSucceed:
+                    Toast.makeText(getApplicationContext(), "참가에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case joinAlreadyIn:
+                    Toast.makeText(getApplicationContext(), "이미 참가하셨습니다.", Toast.LENGTH_SHORT).show();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    };
+}
+class MissionItem implements Serializable{
+    private String UserId;
+    private int recruitId;
+    public String getUserId(){return UserId;}
+    public int getRecruitId(){return recruitId;}
+
+    public MissionItem(String UserId, int recruitId){
+        this.recruitId = recruitId;
+        this.UserId = UserId;
+    }
+
+    public String toString() {
+        return "UserId=" + UserId + "&recruitId=" + Integer.toString(recruitId);
     }
 }
