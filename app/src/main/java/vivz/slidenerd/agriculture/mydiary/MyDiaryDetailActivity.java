da3aac09ed384a9bc9dceaeb954c63e0bacfbc4a
jangwonseo <mklmkl2001@gmail.com>
2015-10-10 19:00:16 +0900

    mydiary detail ui 추가

---
+++ app/src/main/java/vivz/slidenerd/agriculture/mydiary/MyDiaryDetailActivity.java
@@ -0,0 +1,16 @@
+package vivz.slidenerd.agriculture.mydiary;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import vivz.slidenerd.agriculture.R;
+
+public class MyDiaryDetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mydiarydetail);
+    }
+
+}
