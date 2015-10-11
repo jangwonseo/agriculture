488acf5453d0a23bd0b1de305377d5e5e95d3488
김재휘 <zhsks311@naver.com>
2015-10-12 08:25:09 +0900

    참가중인 모집 에러 수정

--- app/src/main/java/vivz/slidenerd/agriculture/mydiary/MyDiaryDetailActivity.java
+++ app/src/main/java/vivz/slidenerd/agriculture/mydiary/MyDiaryDetailActivity.java
@@ -1,16 +1,115 @@
 package vivz.slidenerd.agriculture.mydiary;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import vivz.slidenerd.agriculture.R;
+import vivz.slidenerd.agriculture.list.Item;
 
 public class MyDiaryDetailActivity extends ActionBarActivity {
+
+    Item i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mydiarydetail);
+
+        // mianActivity에서 넘겨준 인텐트정보를 받는다.
+        Intent intent = getIntent();
+        Serializable item = intent.getSerializableExtra("item"); // 클래스를 넘길 때는 Serializable을 이용함
+        // 다이어리에서 넘어오면 1 넘어옴
+
+        i=(Item)item;
+
+
+
+    }
+
+
+
+    // 체험 모집 중인 정보 가져오기
+    public class phpDown extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            StringBuilder jsonHtml = new StringBuilder();
+            String line = "";
+            try {
+                // 텍스트 연결 url 설정
+                URL url = new URL(urls[0]);
+                // 이미지 url
+
+                // URL 페이지 커넥션 객체 생성
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                // 연결되었으면.
+
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setUseCaches(false);
+                    // 연결되었음 코드가 리턴되면.
+                    Log.e("tag", "setUseCaches is false");
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                        for (; ; ) {
+                            // 웹상에 보여지는 텍스트를 라인단위로 읽어 저장.
+                            line = br.readLine();
+                            if (line == null) break;
+                            // 저장된 텍스트 라인을 jsonHtml에 붙여넣음
+                            jsonHtml.append(line);
+
+                        }
+
+                        br.close();
+                    }
+                    conn.disconnect();
+
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return jsonHtml.toString();
+
+
+        }
+
+        protected void onPostExecute(String str) {
+            // JSON 구문을 파싱해서 JSONArray 객체를 생성
+            try {
+
+                JSONArray jAr = new JSONArray(str); // doInBackground 에서 받아온 문자열을 JSONArray 객체로 생성
+
+                for (int i = 0; i < jAr.length(); i++) {  // JSON 객체를 하나씩 추출한다.
+                    JSONObject vilageName = jAr.getJSONObject(i);
+
+
+                    Item item = new Item(vilageName.getString("thumbUrl"), vilageName.getString("name"),
+                            vilageName.getString("adres1"), vilageName.getString("prcafsManMoblphon"),
+                            vilageName.getString("vilageHmpgEnnc"), vilageName.getString("vilageHmpgUrl"),
+                            vilageName.getString("vilageSlgn"), vilageName.getString("tableName"), vilageName.getString("vilageId"));
+                    Log.d("seojang", "정보확인하기 : 끝 ");
+
+                    data.add(item);
+                }
+                vilageList.setAdapter(adapter);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
     }
 
 }
