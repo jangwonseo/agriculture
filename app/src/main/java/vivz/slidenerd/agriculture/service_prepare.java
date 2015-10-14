dc1ed782ec160fbd469cdea16ed2eb1d11cd95b3
jangwonseo <mklmkl2001@gmail.com>
2015-10-14 15:59:59 +0900

    서비스 준비중 작업중

--- app/src/main/java/vivz/slidenerd/agriculture/service_prepare.java
+++ app/src/main/java/vivz/slidenerd/agriculture/service_prepare.java
@@ -1,18 +1,12 @@
 package vivz.slidenerd.agriculture;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
-
-import vivz.slidenerd.agriculture.navigate.NavigateActivity;
-import vivz.slidenerd.agriculture.navigate.navigateSettingPopupActivity;
 
 public class service_prepare extends Activity {
     Button check;
@@ -23,9 +17,11 @@ public class service_prepare extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_prepare);
 
+        preparePopup = (TextView)findViewById(R.id.prepareinfo);
+
+
         yunGothicFont = Typeface.createFromAsset(getAssets(), "fonts/yungothic330.ttf");
         preparePopup.setTypeface(yunGothicFont);
-
         check = (Button) findViewById(R.id.preparecheck);
         check.setOnClickListener(new View.OnClickListener() {
             @Override
