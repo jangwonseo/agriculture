6361e7dab68fcb256f515402584f3603b546dc04
geoncheollee <day071211@gmail.com>
2015-10-16 13:51:35 +0900

    로그아웃 구현 - 원래는 회원가입 하는 곳에서 로그아웃 하려 했으나, fragmentation 을 컨트롤하지 못해서.. 그냥 일반 로그인으로 넘어가서 로그아웃

--- app/src/main/java/vivz/slidenerd/agriculture/sign/NormalLoginActivity.java
+++ app/src/main/java/vivz/slidenerd/agriculture/sign/NormalLoginActivity.java
@@ -1,5 +1,7 @@
 package vivz.slidenerd.agriculture.sign;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,14 +10,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import vivz.slidenerd.agriculture.R;
+import vivz.slidenerd.agriculture.home.HomeActivity;
 
 
 public class NormalLoginActivity extends ActionBarActivity {
     private Typeface yunGothicFont;
     private EditText id_Insert,pw_Insert;
-    private Button nomalLoginBackButton;
+    private Button nomalLoginBackButton, btnLogin;
+
+    //sharedPreference 선언부
+    public SharedPreferences setting;
+    public SharedPreferences.Editor editor;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,39 @@ public class NormalLoginActivity extends ActionBarActivity {
         pw_Insert = (EditText)findViewById(R.id.pw_insert);
         pw_Insert.setTypeface(yunGothicFont);
         //윤고딕 폰트
+
+        //sharedPreference로 전역 공유공간을 만듬
+        setting = getSharedPreferences("setting", MODE_PRIVATE);
+        editor= setting.edit();
+        userId = setting.getString("info_Id", "");
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.linear_login_out);
+        if ( userId.equals("") || userId==null ) {
+            ll.setBackgroundResource(R.drawable.login7);
+        } else {
+            ll.setBackgroundResource(R.drawable.login7_1);
+        }
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ( userId.equals("") || userId == null) { // 로그인을 안했다면
+                    // 로그인 코드
+                } else { // 로그인을 했다면
+                    // 로그아웃
+                    //sharedPreference 입력부분
+                    editor.putString("info_Id", "");
+                    editor.putString("info_Pw", "");
+                    editor.commit();
+                    Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+
+                    Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }
+        });
 
     }
 
