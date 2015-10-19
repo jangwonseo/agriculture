2afccf631bc8d5387420d71883721849b3ae52c0
jangwonseo <mklmkl2001@gmail.com>
2015-10-20 07:01:22 +0900

    test

---
+++ app/src/main/java/vivz/slidenerd/agriculture/list/MapCategoryPopupActivity.java
@@ -0,0 +1,16 @@
+package vivz.slidenerd.agriculture.list;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import vivz.slidenerd.agriculture.R;
+
+public class MapCategoryPopupActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapcategorypopup);
+    }
+
+}
